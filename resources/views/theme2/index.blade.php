a54ecfe2494b2f172b9dc0c2999699d6527b86fd
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2021-09-09 14:59:47 +0600

    item ui implementation

--- resources/views/theme2/index.blade.php
+++ resources/views/theme2/index.blade.php
@@ -2,4 +2,43 @@
 
 @section('content')
   @include('theme2.layout.banner')
+
+
+  <!------------- category start ----------->
+  <section id="category">
+      <div class="container">
+          <ul class="main_category">
+              <li><a href="{{ URL::to('/shop') }}" class="category_button">All items</a></li>
+              <li><a href="#" class="category_button">Scripts</a></li>
+              <li><a href="#" class="category_button">Apps</a></li>
+              <li><a href="#" class="category_button">Themes</a></li>
+              <li><a href="#" class="category_button">Plugins</a></li>
+              <li><a href="#" class="category_button">Graphics</a></li>
+              <li><a href="#" class="category_button">Business</a></li>
+              <li><a href="#" class="category_button">Education</a></li>
+          </ul>
+      </div>
+  </section>
+  <!------------- category end ------------>
+
+
+  <!----------------- latest item start ------------->
+  <section id="latest_item">
+    <div class="container">
+      <div class="title">
+        <h2 class="section_title">Latest Items<span><a href="#">See All</a></span></h2>
+      </div>
+
+      <div class="row">
+        @foreach( $data['itemData']['item'] as $item)
+          @include('theme2.layout.item', [
+            "item" => $item
+          ])
+        @endforeach
+      </div>
+    </div>
+  </section>
+  <!----------------- latest item end ------------->
+
+
 @endsection
---
+++ resources/views/theme2/layout/item.blade.php
@@ -0,0 +1,90 @@
+<div class="col-lg-3">
+  <div class="item_box">
+    <div class="item_view">
+      <div class="item_view_overlay">
+        <div class="item_details">
+          <a href="{{ URL::to('/item') }}/{{ $item->item_slug }}/{{ $item->item_id }}" class="free_item"><i class="fa fa-gift"></i></a>
+          <div class="item_title">
+            <a 
+              href="{{ URL::to('/item') }}/{{ $item->item_slug }}/{{ $item->item_id }}" 
+              class="item_name"
+              title="{{ $item->item_name }}"
+            >
+              {{$item->item_name}}
+            </a>
+          </div>
+        </div>
+
+        <div class="icon_container">
+          <a 
+            href="{{ URL::to('/item') }}/{{ $item->item_slug }}/{{ $item->item_id }}" 
+            data-bs-toggle="tooltip" 
+            data-bs-placement="left" 
+            title="Product details" 
+            class="item_icons"
+          >
+            <i class="fas fa-external-link-alt"></i>
+          </a>
+
+          <!-- @if (Auth::check())
+            @if($item->user_id != Auth::user()->id)
+              <a
+                href="{{ url('/item') }}/{{ base64_encode($item->item_id) }}/favorite/{{ base64_encode($item->item_liked) }}"
+                class="favourite flex-center {{(\Feberr\Models\Items::getLikeCount($item->item_id,  Auth::user()->id)>0)?'item-active-like':''}}"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Add to Favourite"
+              >
+                <i class="fa fa-heart-o"></i>
+              </a>
+            @endif
+          @else
+            <a
+              href="javascript:void(0);"
+              class="favourite flex-center"
+              onClick="alert('Login user only');"
+              data-toggle="tooltip"
+              data-placement="top"
+              title="Add to Favourite"
+            >
+              <i class="fa fa-heart-o"></i>
+            </a>
+          @endif -->
+
+          <a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Save" class="item_icons">
+            <i class="fas fa-folder-plus"></i>
+          </a>
+          <a href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Like" class="item_icons">
+            <i class="fas fa-heart"></i>
+          </a>
+        </div>
+
+      </div>
+      @if($item->item_thumbnail!='')
+        <img class="item_view_img" src="{{ url('/') }}/public/storage/items/{{ $item->item_thumbnail }}" alt="{{ $item->item_name }}">
+      @else
+        <img class="item_view_img" src="{{ url('/') }}/public/img/no-image.jpg" alt="{{ $item->item_name }}">
+      @endif
+    </div>
+
+    <div class="item_bottom">
+      <div class="author_details">
+        <div class="item_author_img">
+          <img src="{{ url('/') }}/public/storage/users/{{ $item->user_photo?$item->user_photo:$item->user->user_photo }}" alt="{{ $item->username}}">
+        </div>
+        <a href="{{ URL::to('/user') }}/{{ $item->username }}">{{ $item->username }}</a>
+      </div>
+      <div class="item_bottom_icons">
+        <a href="#" data-toggle="tooltip" data-placement="bottom" title="Add to Cart" class="item_bottom_cart">
+          <i class="fa fa-cart-plus"></i>
+        </a>
+        <a href="#" data-toggle="tooltip" data-placement="bottom" title="Downloads">
+          <i class="fa fa-download"></i>{{$item->item_sold}}
+        </a>
+        <a href="#" data-toggle="tooltip" data-placement="bottom" title="Likes">
+          <i class="fa fa-heart"></i>300
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
