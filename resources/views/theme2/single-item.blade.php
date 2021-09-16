ce44b724ea15544ff0a1659afd0e284747997d3a
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2021-09-16 15:49:10 +0600

    single item success and warning messages

--- resources/views/theme2/single-item.blade.php
+++ resources/views/theme2/single-item.blade.php
@@ -15,8 +15,42 @@
 
 <section id="single_item_top">
   <div class="container">
-    <div class="row">
+    <div>
+      @if ($message = Session::get('success'))
+      <div class="alert alert-success d-flex justify-content-between align-items-center" role="alert">
+        <span>
+          <span class="alert_icon lnr lnr-checkmark-circle"></span>
+          <span>{{ $message }}</span>
+        </span>
+        <i type="button" class="fas fa-times close" data-bs-dismiss="alert" aria-label="Close"></i>
+      </div>
+      @endif
 
+      @if ($message = Session::get('error'))
+      <div class="alert alert-danger d-flex justify-content-between align-items-center" role="alert">
+        <span>
+          <span class="alert_icon lnr lnr-warning"></span>
+          <span>{{ $message }}</span>
+        </span>
+        <i type="button" class="fas fa-times close" data-bs-dismiss="alert" aria-label="Close"></i>
+      </div>
+      @endif
+
+      @if (!$errors->isEmpty())
+      <div class="alert alert-danger d-flex justify-content-between align-items-center" role="alert">
+        <span>
+          <span class="alert_icon lnr lnr-warning"></span>
+          @foreach ($errors->all() as $error)
+            {{ $error }}
+          @endforeach
+        </span>
+
+        <i type="button" class="fas fa-times close" data-bs-dismiss="alert" aria-label="Close"></i>
+      </div>
+      @endif
+    </div>
+
+    <div class="row">
       <div class="col-lg-6 col-sm-6 col-md-6 col-xl-6 vendor_details_sm">
         <div class="vendor_details">
           <div class="vendor_img">
