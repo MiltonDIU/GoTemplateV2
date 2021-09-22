82973a88971e7952960d929b8f81f3be750c3396
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2021-09-22 10:42:48 +0600

    comment mail content

--- resources/views/theme2/comment_mail.blade.php
+++ resources/views/theme2/comment_mail.blade.php
@@ -1,11 +1,37 @@
-@if($allsettings->maintenance_mode == 0)
+<!DOCTYPE html>
+<html lang="en">
 
-@extends('theme2.layout.master')
+<head>
+  <title>{{ Helper::translation(2905,$translate) }}</title>
+</head>
 
-@section('content')
+<body class="preload dashboard-upload">
 
-@endsection
+  <div class="dashboard_contents">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="dashboard_title_area">
+            <div class="pull-left">
+              <div class="dashboard__title">
+                <h2>{{ Helper::translation(2905,$translate) }}</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-@else
-  @include('theme2.503')
-@endif
+      <div class="row">
+        <div class="col-lg-8 col-md-7">
+          <p><strong> {{ Helper::translation(2906,$translate) }} : </strong> {{ $from_name }}</p>
+          <p><strong> {{ Helper::translation(2907,$translate) }} : </strong> {{ $from_email }}</p>
+          <p><strong> {{ Helper::translation(2908,$translate) }} : </strong> <a href="{{ $item_url }}">{{ $item_url }}</a></p>
+          <p><strong> {{ Helper::translation(2909,$translate) }} : </strong> {{ $comm_text }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
